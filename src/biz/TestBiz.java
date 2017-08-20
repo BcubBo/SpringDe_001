83462c07785b79051f5c8da46b16b1a8f5a1a135
bowieu <fatalitywolf@foxmail.com>
2017-08-20 10:51:30 +0800

    添加jar文件

--- src/biz/TestBiz.java
+++ src/biz/TestBiz.java
@@ -1,5 +1,7 @@
 package biz;
 
+import dao.DaoFactory;
+import dao.ITestDao;
 import dao.TestDao;
 
 public class TestBiz {
@@ -8,7 +10,7 @@ public class TestBiz {
 	public void testBiz() {
 		
 		
-		TestDao dao = new TestDao();
+		ITestDao dao = DaoFactory.getInstance();
 		dao.testDao();
 		
 		
--- src/dao/NewDao.java
+++ src/dao/NewDao.java
@@ -4,7 +4,7 @@ public class NewDao implements ITestDao {
 
 	@Override
 	public void testDao() {
-		
+		System.out.println("new implements class NewDao");
 
 	}
 
