2a85be4d16d095d3f26d36b2b6e321974398792b
bowieu <fatalitywolf@foxmail.com>
2017-08-20 11:14:36 +0800

    配置文件的书写

--- src/biz/TestBiz.java
+++ src/biz/TestBiz.java
@@ -6,14 +6,33 @@ import dao.TestDao;
 
 public class TestBiz {
 	
+	private ITestDao dao;
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	public void testBiz() {
 		
 		
-		ITestDao dao = DaoFactory.getInstance();
+//		ITestDao dao = DaoFactory.getInstance();
+		System.out.println("执行业务逻辑");
 		dao.testDao();
 		
 		
 	}
-
+	//某方法
+	public ITestDao getDao() {
+		return dao;
+	}//
+	public void setDao(ITestDao dao) {
+		this.dao = dao;
+	}//
+	
+	
 }
