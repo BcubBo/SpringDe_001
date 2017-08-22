73f782e92a110b3d16343aa76d2315a711808af9
bowieu <fatalitywolf@foxmail.com>
2017-08-22 14:20:03 +0800

    将log4j1更改为log4j2

--- src/Logger/testLogger.java
+++ src/Logger/testLogger.java
@@ -2,8 +2,11 @@ package Logger;
 
 import org.apache.log4j.Logger;
 
+import biz.TestBiz;
+
+
 public class testLogger {
-	Logger logger = Logger.getLogger(testLogger.class);
+	Logger logger = Logger.getLogger(TestBiz.class);
 	
 	public void beforeLogger() {
 		
