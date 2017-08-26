e568052806d5f0e3b8039966ee8a743c11130123
bowieu <fatalitywolf@foxmail.com>
2017-08-26 14:00:46 +0800

    使用各种注入的方式

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,10 +2,17 @@ package test;
 
 
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 import dao.ITestBiz;
 
@@ -22,7 +29,26 @@ public class Test {
 		//biz.init();
 		biz.testBiz("测试JointPoint连接点信息class使用","hiThere");
 		//new FileSystemXmlApplicationContext("");如果文件没有在src文件夹下
-		//sdfsdf
+		
+		
+		Map<String,Object> map = new HashMap<String,Object>();
+		//import java.util.Entry;
+		Set<Entry<String,Object>> entry = map.entrySet();
+		//打包发送回entrySet
+		Iterator<Entry<String,Object>> it = entry.iterator();
+		
+		while(it.hasNext()) {
+			
+			Entry<String,Object> one = it.next();
+			//迭代获取Entry对象
+			one.getKey();
+			//获取Entry对象的key
+			one.getValue();
+			//获取Entry对象的value
+			
+			
+		}
+		
 	}
 
 }
